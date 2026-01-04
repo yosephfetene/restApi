b1d1cff8277292430501c0fa4e83a71741297941
yosephfetene <155078686+yosephfetene@users.noreply.github.com>
2026-01-04 23:46:43 +0300

    excercise

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 import express from "express";
 import https from "https";
+
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("Server is up and running"); 
-})
+app.get("/", function (req, res) {
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=d1ddb2692818c1e61db3de8a7f683383&units=metric";
 
-app.listen(3000, function(){
+    https.get(url, function (response) {
+        console.log(response.statusCode); 
+
+        response.on("data", function (data) {
+            const weatherData = JSON.parse(data);
+            console.log(weatherData); 
+
+            const object = {
+                name: "Angela",
+                m8: "MKBHD"
+            };
+
+            console.log(JSON.stringify(object));
+        });
+    });
+
+    res.send("Server is up and running");
+});
+
+app.listen(3000, function () {
     console.log("The server is running on port 3000.");
 });
